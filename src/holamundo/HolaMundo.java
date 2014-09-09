95acbdc4e6fd2232d3c582e8c613bc8d61e6b1f2
mgarciav <manigave@gmail.com>
2014-09-09 13:56:51 -0300

    updateo 1.1
    asdf

--- src/holamundo/HolaMundo.java
+++ src/holamundo/HolaMundo.java
@@ -16,7 +16,7 @@ public class HolaMundo {
      * @param args the command line arguments
      */
     public static void main(String[] args) {
-        System.out.println("Hola Mundo");
+        System.out.println("Hola Mundo!");
         // TODO code application logic here
     }
     
